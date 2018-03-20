273905357e483fd71be5eabc722e8a35586f0929
DESKTOP-Q76JGGA\xiang <xiangwei71@gmail.com>
2018-03-20 14:18:14 +0800

    Initial commit

---
+++ DiyAStar/AStar/IGraphNode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DiyAStar
+{
+    public interface IGraphNode
+    {
+        void setVisited();
+        bool isVisited();
+        int EdgeCount();
+        IGraphNode GetEdge(int index);
+        float GetEdgeCost(int index);
+        float getEvaluation(IGraphNode target);
+        float getAccumulationCost();
+        void setAccumulationCost(float pCost);
+        void setComeFrom(IGraphNode node);
+        IGraphNode getComeFrom();
+        string getNodeKey();
+    }
+}
+
